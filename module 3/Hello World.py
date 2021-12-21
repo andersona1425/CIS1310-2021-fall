58bfd22f84900c9551c2e0a67ff345a3a8dd1031
Anderson A <andersona1425@dupage.edu>
2021-12-21 03:46:09 -0600

    fixed python

--- module 3/Hello World.py
+++ module 3/Hello World.py
@@ -1,7 +1,14 @@
-def mysum(*numbers):
-    result = 0
-    for number in numbers:
-        result += number
-    return result
+x = int(input("What year is it?"))
+y = int(input("What year were you born?"))
 
-print(mysum(10, 20, 30))
+try: 
+    birth_year = x - y
+    print("The amount of years you were born is or a year off from....")
+    print(birth_year)
+
+except ValueError:
+    print("Please enter a valid key. Numbers only.")
+    x = 2021 
+    input("What year were you born?")
+except:
+    print("an error has occured")
